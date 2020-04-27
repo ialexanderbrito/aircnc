6cbb6cfef85f615a9bd0f6fd150c40b6961ff709
ialexanderbrito <xanderzinho26@gmail.com>
2020-04-27 19:00:41 -0300

    Adicionado CORS e outras alterações

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
 const app = express();
 
 mongoose.connect(
-    'mongodb+srv://alexander:omnistack@cluster0-gwhau.mongodb.net/omnistack?retryWrites=true&w=majority', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }
+  'mongodb+srv://alexander:omnistack@cluster0-gwhau.mongodb.net/omnistack?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
 );
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
