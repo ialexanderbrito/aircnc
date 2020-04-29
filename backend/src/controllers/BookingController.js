9921ade04d18e2cfee7d20dbd988cfac18ef70d5
ialexanderbrito <xanderzinho26@gmail.com>
2020-04-29 19:32:29 -0300

    Adicionado Socket.io

--- backend/src/controllers/BookingController.js
+++ backend/src/controllers/BookingController.js
@@ -1,19 +1,25 @@
 const Booking = require('../models/Booking');
 
 module.exports = {
-    async store(request, response) {
-        const { user_id } = request.headers;
-        const { spot_id } = request.params;
-        const { date } = request.body;
+  async store(request, response) {
+    const { user_id } = request.headers;
+    const { spot_id } = request.params;
+    const { date } = request.body;
 
-        const booking = await Booking.create({
-            user: user_id,
-            spot: spot_id,
-            date,
-        });
+    const booking = await Booking.create({
+      user: user_id,
+      spot: spot_id,
+      date,
+    });
 
-        await booking.populate('spot').populate('user').execPopulate();
+    await booking.populate('spot').populate('user').execPopulate();
 
-        return response.json(booking);
-    },
+    const ownerSocket = request.connectedUsers[booking.spot.user];
+
+    if (ownerSocket) {
+      request.io.to(ownerSocket).emit('booking_request', booking);
+    }
+
+    return response.json(booking);
+  },
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,9 +3,14 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io');
+const http = require('http');
+
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect(
   'mongodb+srv://alexander:omnistack@cluster0-gwhau.mongodb.net/omnistack?retryWrites=true&w=majority',
@@ -15,9 +20,24 @@ mongoose.connect(
   }
 );
 
+const connectedUsers = {};
+
+io.on('connection', (socket) => {
+  const { user_id } = socket.handshake.query;
+
+  connectedUsers[user_id] = socket.id;
+});
+
+app.use((request, response, next) => {
+  request.io = io;
+  request.connectedUsers = connectedUsers;
+
+  return next();
+});
+
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
