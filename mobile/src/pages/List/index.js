721c1ef60c2af2f00de2318f93e85681ce03ee0a
ialexanderbrito <xanderzinho26@gmail.com>
2020-04-29 19:40:01 -0300

    Adicionado Socket.io-client e Alert box

--- mobile/src/pages/List/index.js
+++ mobile/src/pages/List/index.js
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, ScrollView, Image, AsyncStorage } from 'react-native';
+import socketio from 'socket.io-client';
+import {
+  Alert,
+  SafeAreaView,
+  ScrollView,
+  Image,
+  AsyncStorage,
+} from 'react-native';
 
 import SpotList from '../../components/SpotList';
 
@@ -9,6 +16,22 @@ import styles from './styles';
 
 export default function List() {
   const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem('user').then((user_id) => {
+      const socket = socketio('http://192.168.100.4:3333', {
+        query: { user_id },
+      });
+
+      socket.on('booking_response', (booking) => {
+        Alert.alert(
+          `Sua reserva em ${booking.spot.company} em ${booking.date} foi ${
+            booking.approved ? 'APROVADA' : 'REJEITADA'
+          }`
+        );
+      });
+    });
+  });
 
   useEffect(() => {
     AsyncStorage.getItem('techs').then((storageTechs) => {
