c87048eceee18873d2484db03f8eb8bfc49ad363
Alexander <xanderzinho26@gmail.com>
2022-08-26 22:40:32 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://aircnc-oficial.herokuapp.com',
+  baseURL: 'https://aircnc-api.ialexanderbrito.dev',
 });
 
 export default api;
